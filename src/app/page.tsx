16819bfc57abdd99362a6634ef9a0a638ecf0b52
shadab-gtf <shadab@gtftechnologies.com>
2026-01-24 12:20:09 +0530

    scroll video uncomment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
      <Header/>
      <Hero/>
      <About/>
-     {/* <ScrollVideoCanvas/> */}
+     <ScrollVideoCanvas/>
      <LiveParallaxBackground/>
      <Vision/>
      <Purpose/>
